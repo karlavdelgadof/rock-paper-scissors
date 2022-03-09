ae70c00707181d35bab48d0a8278521fe299e215
Karla <karlavdelgadof@gmail.com>
2022-03-09 08:41:32 -0500

    Delete parameters, change return & uncomment func

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const computerSelection = computerPlay();
 //console.log(computerSelection)
 function computerPlay() {
     const options = ['rock', 'paper', 'scissors'];
@@ -15,48 +14,45 @@ function askUser() {
 let playerScore = 0;
 let computerScore = 0;
 
-function playRound(playerSelection, computerSelection) {
-        switch (true) {
-            case (playerSelection === 'rock' && computerSelection === 'scissors'):
-            case (playerSelection === 'paper' && computerSelection === 'rock'):
-            case (playerSelection === 'scissors' && computerSelection === 'paper'):
-                return `You've won this round! ${playerSelection} beats ${computerSelection}.
-                Computer: ${computerScore}
-                You: ${++playerScore}`;
-                break;
-            case (playerSelection === 'rock' && computerSelection === 'paper'):
-            case (playerSelection === 'paper' && computerSelection === 'scissors'):
-            case (playerSelection === 'scissors' && computerSelection === 'rock'):
-                return `You've lost this round. ${computerSelection} beats ${playerSelection}.
-                Computer: ${++computerScore}
-                You: ${playerScore}`;
-                break;
-            case (playerSelection === computerSelection):
-                return `It's a draw!`;
-                break;
-            default:
-                return `Please play either rock, paper or scissors.`;
-        };
+function playRound() {
+    const computerSelection = computerPlay();
+    askUser();
+    switch (true) {
+        case (playerSelection === 'rock' && computerSelection === 'scissors'):
+        case (playerSelection === 'paper' && computerSelection === 'rock'):
+        case (playerSelection === 'scissors' && computerSelection === 'paper'):
+            console.log( `You've won this round! ${playerSelection} beats ${computerSelection}.\nComputer: ${computerScore}\nYou: ${++playerScore}`);
+        break;
+        case (playerSelection === 'rock' && computerSelection === 'paper'):
+        case (playerSelection === 'paper' && computerSelection === 'scissors'):
+        case (playerSelection === 'scissors' && computerSelection === 'rock'):
+            console.log(`You've lost this round. ${computerSelection} beats ${playerSelection}.\nComputer: ${++computerScore}\nYou: ${playerScore}`);
+        break;
+        case (playerSelection === computerSelection):
+            console.log(`It's a draw!`);
+        break;
+        default:
+            console.log( `Please play either rock, paper or scissors.`);
+    };
 };
 
+//console.log(playRound())
 
 
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         console.log(i)
-//         askUser();
-//         playRound(playerSelection, computerSelection);
-//         console.log(computerSelection);
-//         if (playerScore >= 3 || computerScore >= 3) {
-//             break;
-//         } else {
-//         };        
-//     };
-//     if (playerScore > computerScore) {
-//         return `You've won the game!`;
-//     } else {
-//         return `You've lost. Computer won this time.`;
-//     };
-// };
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
 
+        if (playerScore >= 3 || computerScore >= 3) {
+            break;
+        };        
+    };
+    if (playerScore > computerScore) {
+        return `You've won the game!`;
+    } else {
+        return `You've lost. Computer won this time.`;
+    };
+};
+
+console.log(game())
 
